29ce3c2e8c34f978c966babb65158c13bf1aca2f
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2024-03-15 11:19:45 -0300

    agrego midlewares y comentarios

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const server =  require("./src/app");
 const port = "3001";
 
+//levanto el servidor
 server.listen(port, () => {
     console.log(`server listening on port ${port}`);
 });
--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,13 @@
+//requerimientos 
 const express = require("express");
+const morgan = require("morgan");
+
+//creo el servidor
 const server = express();
+
+//midlewares
+server.use(express.json());
+server.use(morgan("dev"));
+
 
 module.exports = server;
